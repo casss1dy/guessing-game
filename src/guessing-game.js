fd35cea59f62109205fcd7092c79af32ec8736b3
Nadya Khramova <casssidy@ya.ru>
2019-10-06 01:31:42 +0400

    feat: add guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.range = {
+            min,
+            max
+        };
     }
 
     guess() {
-
+        return this.candidat = Math.ceil((this.range.min + this.range.max)/2);
     }
 
     lower() {
-
+        this.range.max = this.candidat;
     }
 
     greater() {
-
+        this.range.min = this.candidat;
     }
 }
 
